19af533fc86c890e81bf943149d2efd6eebc8506
pedrogallon <pedrogallonalves@hotmail.com>
2020-10-18 16:00:35 -0300

    innitial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -27,
+          longitude: -49.0,
+          latitudeDelta: 0,
+          longitudeDelta: 0,
+        }}
+
+
+
+      />
     </View>
   );
 }
@@ -18,4 +31,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  map:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  }
 });
